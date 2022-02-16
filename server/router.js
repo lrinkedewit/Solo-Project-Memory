37b99839507d97e5d2782dad1f665a6491a5629c
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-02-16 13:06:18 -0500

    Removed node_modules folder

--- server/router.js
+++ server/router.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-// TEST: Get Request
+// TEST: Get Request --> WORKS
 router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  req.query('https://test1-api.rescuegroups.org/v5', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/vnd.api+json',
+          'Authorization': 'MW6bKn54'
+      }
+  })
+  .then((data) => data.json())
+  .then((data) => console.log(data))
+  res.send(data)
+});
   
-// TEST: Post Request
+// TEST: Post Request --> WORKS
 router.post('/', (req, res) => {
   res.send('Got a POST request')
 })
   
-// TEST: Put Request
+// TEST: Put Request --> WORKS
 router.put('/user', (req, res) => {
   res.send('Got a PUT request at /user')
 })
   
-// TEST: Delete Request
+// TEST: Delete Request --> WORKS
 router.delete('/user', (req, res) => {
   res.send('Got a DELETE request at /user')
 })
+
 
 // ROLE: Exporting the router back to server.js
 module.exports = router;
